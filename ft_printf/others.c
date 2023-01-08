83ea7fb430187e9929cf923ed88f341c12e78cf9
mohammed <71414472+mohammedhrima@users.noreply.github.com>
2023-01-08 07:39:34 +0100

    Add files via upload

--- ft_printf/others.c
+++ ft_printf/others.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   others.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhrima <mhrima@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/08 13:25:32 by mhrima            #+#    #+#             */
-/*   Updated: 2023/01/03 12:40:19 by mhrima           ###   ########.fr       */
+/*   Created: 2022/11/06 10:44:11 by mhrima            #+#    #+#             */
+/*   Updated: 2022/11/06 10:47:43 by mhrima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-size_t	ft_strlen(const char *str)
+int	print_space(int *addSpace)
 {
 	int	i;
 
 	i = 0;
-	while (str && str[i])
-		i++;
+	while ((*addSpace)-- > 0)
+		i += ft_putchar(' ');
 	return (i);
 }
